83a393a5ea698026e0b93cc930706cd0016d9d5a
RobertaBtt <roberta.btt@gmail.com>
2021-05-12 16:33:55 +0200

    Test filters on API

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,19 +1,32 @@
 from unittest import TestCase
 import requests
 
+
 class TestApi(TestCase):
 
     def setUp(self) -> None:
         self.api_end_point = "https://jsonplaceholder.typicode.com/todos"
-        self.params = {"_start": str(45), "_limit": str(1)}
+
+        self.params = {"_start": str(45),
+                       "_limit": str(1)}
+
+        self.params_filters = {"completed": "false",
+                               "_limit": str(10),
+                               "_start": str(9)}
 
     def test_api_is_online(self):
-        response =  requests.get(self.api_end_point)
+        response = requests.get(self.api_end_point)
         assert (response.status_code == 200)
 
     def test_api_has_pagination(self):
+        # Asking for https://jsonplaceholder.typicode.com/todos?completed=false&?_start=9&_limit=10
+        response = requests.get(self.api_end_point, params=self.params_filters)
+        assert (response.status_code == 200)
+        assert (len(response.json()) == 10)
 
-        # Asking for:
+
+    def test_api_has_filters(self):
+        # Asking for: "https://jsonplaceholder.typicode.com/todos?_start=45&_limit=1"
         response = requests.get(self.api_end_point, params=self.params)
 
         assert (response.status_code == 200)
