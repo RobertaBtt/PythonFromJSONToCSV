a1b400e87a5778fd26bf95a9d91e2385749839f6
RobertaBtt <roberta.btt@gmail.com>
2021-05-12 19:00:58 +0200

    TDD test simple file created and tested

--- tests/test_file.py
+++ tests/test_file.py
@@ -7,14 +7,30 @@ class TestFile(TestCase):
     def setUp(self) -> None:
         self.name_file = "2021_05_12_999.csv"
         self.header_csv = "userId, id,title,completed"
+        self.content_test = "1,1,delectus aut autem,false"
 
-    def test_file_has_been_created(self):
+    def test_file_created_just_header(self):
         # Temporary text file in write mode
-        temp_file = TemporaryFile('w+t')
-        temp_file.write(self.header_csv)
+        with TemporaryFile('w+t') as temp_file:
+            temp_file.write(self.header_csv)
 
-        # Begin of the file
-        temp_file.seek(0)
-        data = temp_file.read()
-        temp_file.close()
-        assert (data == self.header_csv)
+            # Begin of the file
+            temp_file.seek(0)
+            data = temp_file.read()
+            temp_file.close()
+            assert (data == self.header_csv)
+
+    def test_file_created_header_and_content(self):
+        # Temporary text file in write mode
+        with TemporaryFile('w+t') as temp_file:
+
+            temp_file.write(self.header_csv +"\n")
+            temp_file.write(self.content_test)
+
+            # Begin of the file
+            temp_file.seek(0)
+
+            first_line = temp_file.readline()
+            second_line = temp_file.readline()
+            assert (first_line == self.header_csv+"\n")
+            assert (second_line == self.content_test)
